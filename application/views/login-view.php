86ef10446ff25aebf8768cf7a9f2fd1977675caa
Gabriel Delbão de Oliveira <gabriel.delbao@yahoo.com.br>
2015-03-13 12:56:12 -0300

    News
    Diversas Alterações

--- application/views/login-view.php
+++ application/views/login-view.php
@@ -3,11 +3,17 @@
     <head>
         <meta charset="UTF-8">
         <title>Login::CI</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('utility/css/login.css') ?>"
     </head>
     <body>
         <h1>Login de Usuário</h1>
-        <?php
-        // put your code here
-        ?>
+        <form method="post" action="">
+            <label for="txtLogin">Login</label>
+            <input type="text" name="txtLogin"/>
+            <label for='txtSenha'>Senha</label>
+            <input type="password" name="txtSenha"/>
+            <input type="submit" value="Entrar"/>
+            <input type="reset" value="Cancelar"/>
+        </form>
     </body>
 </html>
