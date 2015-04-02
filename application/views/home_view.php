b8c42c65bdf1dfd69ee09cf05653ca1125758c99
Gabriel Delbão de Oliveira <gabriel.delbao@yahoo.com.br>
2015-04-02 12:09:50 -0300

    Teste de Commit sem Metadados do Netbeans

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Área Privada</title>
+        <title>Área Privada do Site</title>
     </head>
     <body>
         <h1>home</h1>
